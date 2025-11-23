32e3728b28384ceb37b1e1b9a31c24aff0eb2adb
jaaaa8 <bileyghas1eye@gmail.com>
2025-11-23 16:07:00 +0700

    update db

--- database/bookstore.sql
+++ database/bookstore.sql
@@ -1,3 +1,4 @@
+DROP database case_std_bookstore_db;
 CREATE DATABASE IF NOT EXISTS case_std_bookstore_db;
 USE case_std_bookstore_db;
 
@@ -21,9 +22,15 @@ CREATE TABLE Customer (
 
 CREATE TABLE Author (
     author_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(150) NOT NULL,
-    bio TEXT,
-    email VARCHAR(100) UNIQUE
+    name VARCHAR(150) UNIQUE NOT NULL,
+    bio TEXT
+);
+
+CREATE TABLE Publisher (
+    publisher_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(150) UNIQUE NOT NULL,
+    address VARCHAR(255),
+    phone VARCHAR(20)
 );
 
 CREATE TABLE Category (
@@ -34,40 +41,27 @@ CREATE TABLE Category (
 CREATE TABLE Book (
     book_id INT PRIMARY KEY AUTO_INCREMENT,
     category_id INT,
-    author_id INT,
+    author_id INT NOT NULL,
+    publisher_id INT NOT NULL,
     title VARCHAR(150) NOT NULL,
     description TEXT,
     price DECIMAL(10,2) NOT NULL,
     stock INT DEFAULT 0,
     image_url VARCHAR(255),
+
     FOREIGN KEY (category_id) REFERENCES Category(category_id),
-    FOREIGN KEY (author_id) REFERENCES Author(author_id)
-);
-
-CREATE TABLE Cart (
-    cart_id INT PRIMARY KEY AUTO_INCREMENT,
-    account_id INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (account_id) REFERENCES Account(account_id)
-);
-
-CREATE TABLE CartItem (
-    cart_item_id INT PRIMARY KEY AUTO_INCREMENT,
-    cart_id INT,
-    book_id INT,
-    quantity INT NOT NULL DEFAULT 1,
-    FOREIGN KEY (cart_id) REFERENCES Cart(cart_id),
-    FOREIGN KEY (book_id) REFERENCES Book(book_id)
+    FOREIGN KEY (author_id) REFERENCES Author(author_id),
+    FOREIGN KEY (publisher_id) REFERENCES Publisher(publisher_id)
 );
 
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT,
-    status ENUM('PENDING','CONFIRMED','SHIPPED','CANCELLED','COMPLETED') 
-        DEFAULT 'PENDING',
+    status ENUM('CART','PENDING','CONFIRMED','SHIPPED','COMPLETED','CANCELLED')
+        DEFAULT 'CART',
     total DECIMAL(10,2),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    confirmed_by INT NULL,
+    confirmed_by INT,
     FOREIGN KEY (customer_id) REFERENCES Customer(customer_id),
     FOREIGN KEY (confirmed_by) REFERENCES Account(account_id)
 );
@@ -82,12 +76,3 @@ CREATE TABLE OrderItem (
     FOREIGN KEY (book_id) REFERENCES Book(book_id)
 );
 
-CREATE TABLE OrderHistory (
-    history_id INT PRIMARY KEY AUTO_INCREMENT,
-    order_id INT,
-    action VARCHAR(100),
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    by_account INT,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (by_account) REFERENCES Account(account_id)
-);
